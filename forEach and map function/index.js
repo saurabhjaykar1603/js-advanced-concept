878c98800d929a281841ba13e2e904a8a0d01a87
saurabh jaykar <saurabh@kali.kali.org>
2023-09-19 23:05:35 +0530

    code opt

--- forEach and map function/index.js
+++ forEach and map function/index.js
@@ -1,11 +1,15 @@
 const students = ["Saurabh", "Yogita", "Harsh"];
 
-const sayHello = (studName) => {
-  console.log(`Hello ${studName}`);
-};
+// const sayHello = (studName) => {
+//   console.log(`Hello ${studName}`);
+// };
 
 // for (let i = 0; i < students.length; i++) {
 //   sayHello(students[i]);
 // }
 
-students.forEach(sayHello);
+// students.forEach();
+
+students.forEach((studName) => {
+  console.log(`Hello ${studName}`);
+});
