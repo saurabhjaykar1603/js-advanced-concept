c3434c8c8d80cdfe0db8f8e419e8d71c715b10b3
saurabh jaykar <saurabh@kali.kali.org>
2023-09-19 14:26:12 +0530

    initial changes

--- useEffect, iife  and arrow functions/index.js
+++ useEffect, iife  and arrow functions/index.js
@@ -3,22 +3,24 @@
 // internal working of useEffect
 
 function useEffect(fn, dependencies) {
-    console.log("useEffect");   // Log "useEffect" to the console
-    fn();                       // Call the function 'fn'
-    console.log(dependencies);  // Log the 'dependencies' array to the console
-    console.log("useEffect end"); // Log "useEffect end" to the console
-  }
-  
+  console.log("useEffect"); // Log "useEffect" to the console
+  fn(); // Call the function 'fn'
+  console.log(dependencies); // Log the 'dependencies' array to the console
+  console.log("useEffect end"); // Log "useEffect end" to the console
+}
 
-useEffect(function SayHello() {
-  console.log(" call back function Hello");
-}, ["hello", "world" ]);
+useEffect(
+  function SayHello() {
+    console.log(" call back function Hello");
+  },
+  ["hello", "world"]
+);
 
-
-
-useEffect( SayHello = () => {
+useEffect(
+  (SayHello = () => {
     console.log(" arrow function Hello");
-  }, ["hello", "world" ]);
-  
+  }),
+  ["hello", "world"]
+);
 
 // This code is a simplified implementation of a custom useEffect function. When called, it logs "useEffect," executes a provided function (in this case, SayHello), logs an array of dependencies (["hello", "world"]), and logs "useEffect end." As a result, "Hello" and the dependencies are displayed in the console.
