e5b662cc32618b29e82b3ff57de9a1df16929d30
saurabh jaykar <saurabh@kali.kali.org>
2023-09-19 14:22:22 +0530

    learned - internal working useEffect

---
+++ useEffect, iife  and arrow functions/index.js
@@ -0,0 +1,17 @@
+// useEffect vanila js code
+
+// internal working of array 
+
+function useEffect(fn, dependencies) {
+    console.log("useEffect");   // Log "useEffect" to the console
+    fn();                       // Call the function 'fn'
+    console.log(dependencies);  // Log the 'dependencies' array to the console
+    console.log("useEffect end"); // Log "useEffect end" to the console
+  }
+  
+
+useEffect(function SayHello() {
+  console.log("Hello");
+}, ["hello", "world" ]);
+
+// This code is a simplified implementation of a custom useEffect function. When called, it logs "useEffect," executes a provided function (in this case, SayHello), logs an array of dependencies (["hello", "world"]), and logs "useEffect end." As a result, "Hello" and the dependencies are displayed in the console.
